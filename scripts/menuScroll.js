3c2dbf11cccd59b5c46060c88555f25c6955f8c9
Richard <89549164+mastarious667@users.noreply.github.com>
2022-06-03 06:14:25 +0200

    Same changes that are in index

--- scripts/menuScroll.js
+++ scripts/menuScroll.js
@@ -1,20 +1,45 @@
-const HEADER = document.querySelector("#header")
+const header = document.querySelector("#header")
+const title = document.querySelector("#title")
+const menuElements = document.querySelectorAll(".menu-element")
+
 
 window.onscroll = function() {scroll()}
 
+
+console.log(menuElements)
 function scroll()
 {
     if(document.body.scrollTop > 5 || document.documentElement.scrollTop > 5)
     {
-        HEADER.classList.add("header-fixed")
-        HEADER.classList.add("header-dark")
-        // HEADER.classList.add("header-smaller")
+        header.classList.add("header-fixed")
+        header.classList.add("header-dark")
+
+        header.classList.add("header-container-smaller")
+        header.classList.remove("header-container")
+
+        title.classList.add("title-smaller")
+        title.classList.remove("title")
+
+        menuElements.forEach(menuElement => {
+            menuElement.classList.remove("menu-element")
+            menuElement.classList.add("menu-element-smaller")
+        });
     }
 
     else
     {
-        HEADER.classList.remove("header-fixed")
-        HEADER.classList.remove("header-dark")
-        // HEADER.classList.remove("header-smaller")
+        header.classList.remove("header-fixed")
+        header.classList.remove("header-dark")
+
+        header.classList.remove("header-container-smaller")
+        header.classList.add("header-container")
+
+        title.classList.remove("title-smaller")
+        title.classList.add("title")
+
+        menuElements.forEach(menuElement => {
+            menuElement.classList.remove("menu-element-smaller")
+            menuElement.classList.add("menu-element")
+        });
     }
 }
