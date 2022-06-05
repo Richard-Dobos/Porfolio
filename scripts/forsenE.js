bf52a6effacfda0b3bb92ae688ccc972da37375e
Richard <89549164+mastarious667@users.noreply.github.com>
2022-06-05 21:17:29 +0200

    Made working :tf:

--- scripts/forsenE.js
+++ scripts/forsenE.js
@@ -1,31 +1,63 @@
-let image = `<img src="../src/images/forsenE.png" class="forsenE forsenE-display-none" id="forsen">`
-
-document.querySelector("#body").innerHTML += image
+const image = `<img src="../src/images/forsenE.png" class="forsenE" id="forsen">`
 
 let w = window.innerWidth
 let h = window.innerHeight
 
+let audioPlayed = false
 let forsenE
 
 spawn()
 
-forsenE.addEventListener("click", playAudio)
-
 function spawn()
 {
+    document.querySelector("#body").innerHTML += image
+    
     forsenE = document.querySelector("#forsen")
-
+    
+    
     const randomSpawnX = Math.random() * w + 1
     const randomSpawnY = Math.random() * h + 1
-
+    
     console.log(`x: ${randomSpawnX}\ny: ${randomSpawnY}`)
-
-    forsenE.classList.remove("forsenE-display-none")
-
+    
     forsenE.style = `top: ${randomSpawnY}px;left: ${randomSpawnX}px;`
+    
+    forsenE.addEventListener("click", playAudioSequence)
+
+    const timeout = Math.random() * 30000 + 2500
+
+    console.log(timeout)
+
+    setTimeout(liveTime, timeout)
+}
+
+function liveTime()
+{
+    despawn()
+}
+
+function playAudioSequence()
+{
+    playAudio()
+    setTimeout(removeAudio, 5000)
+    audioPlayed = true
+    despawn()
+    forsenE = document.querySelector("#forsen")
+}
+
+function despawn()
+{
+    document.querySelector("#forsen").remove()
+    spawn()
 }
 
 function playAudio()
 {
-    document.querySelector("#body").innerHTML += `<audio src="../src/never_lucky_man.mp3" autoplay></audio>`
+    document.querySelector("#body").innerHTML += `<audio src="../src/never_lucky_man.mp3" id="audio" autoplay></audio>`
+}
+
+function removeAudio()
+{
+    document.querySelector("#audio").remove()
+    audioPlayed = false
 }
